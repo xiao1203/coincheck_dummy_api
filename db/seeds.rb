a3e335810db2989f05c9d4166f8fcc1c0578a4d9
Yokoyama <x.xiao.turquoise@gmail.com>
2017-05-20 11:23:18 +0900

    seedファイル周りの設定を更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 include SeedModule
 
 ActiveRecord::Base.transaction do
-  SeedModule.import("ticker")
-  SeedModule.import("trade")
-  SeedModule.import("order_book")
-  SeedModule.import("rate")
+  SeedModule.import("ticker", delete_all: true)
+  SeedModule.import("trade", delete_all: true)
+  SeedModule.import("order_book", delete_all: true)
+  SeedModule.import("rate", delete_all: true)
 end
