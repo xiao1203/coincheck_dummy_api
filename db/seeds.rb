2e3ab7356e99153cd004f3f2a012d01a6474191e
Yokoyama <x.xiao.turquoise@gmail.com>
2017-06-23 23:38:50 +0900

    seed対象変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@ ActiveRecord::Base.transaction do
   SeedModule.import("order_book", delete_all: true)
   SeedModule.import("rate", delete_all: true)
 
-  SeedModule.import("zaif_ticker", delete_all: true)
-  SeedModule.import("zaif_trade", delete_all: true)
-  SeedModule.import("zaif_depth", delete_all: true)
-
-  SeedModule.import("bitflyer_ticker", delete_all: true)
-  SeedModule.import("bitflyer_board", delete_all: true)
-  SeedModule.import("bitflyer_execution", delete_all: true)
+  # SeedModule.import("zaif_ticker", delete_all: true)
+  # SeedModule.import("zaif_trade", delete_all: true)
+  # SeedModule.import("zaif_depth", delete_all: true)
+  #
+  # SeedModule.import("bitflyer_ticker", delete_all: true)
+  # SeedModule.import("bitflyer_board", delete_all: true)
+  # SeedModule.import("bitflyer_execution", delete_all: true)
 end
